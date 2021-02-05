f95af0208b2c9e66f3067d21c1c63ad41b1e91e0
MarizzaM <75089445+MarizzaM@users.noreply.github.com>
2021-02-05 14:26:22 +0200

    Add files via upload

---
+++ HWLesson26/MyProtectedUniqueList.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HWLesson26
+{
+    class MyProtectedUniqueList<T> : IEnumerable<T>
+    {
+        private List<T> words = new List<T>();
+        private string secretWord;
+
+        public MyProtectedUniqueList(string secretWord)
+        {
+            this.secretWord = secretWord;
+        }
+
+        public void Add(T word)
+        {
+            if (word == null)
+                throw new ArgumentNullException($"Argument Exception! Word is '{word}'");
+
+            if (words.Contains(word))
+                throw new InvalidOperationException($"Word '{word}' already exists in the list ");
+            else
+                words.Add(word);
+        }
+
+        public void Remove(T word)
+        {
+            if (word == null)
+                throw new ArgumentNullException($"Argument Exception! Word is '{word}'");
+
+            if (!words.Contains(word))
+                throw new ArgumentException($"The word '{word}' is not in the initialization list");
+            else
+                words.Remove(word);
+        }
+
+        public void RemoveAt(int index)
+        {
+            if (index > words.Count  && index < 0)
+                throw new ArgumentOutOfRangeException($"Index {index} out of range ");
+            else
+                words.Remove(words[index]);
+        }
+
+        public void Clear(string word) {
+            if (word != this.secretWord)
+                throw new AccessViolationException($"The word {word} is not the key ");
+            else
+                words.Clear();
+        }
+
+        public void Sort(string word)
+        {
+            if (word != this.secretWord)
+                throw new AccessViolationException($"The word {word} is not the key ");
+            else
+                words.Sort();
+        }
+
+        public override string ToString()
+        {
+            string result = "";
+
+            foreach (T word in words)
+            {
+                result += word + "\n";
+            }
+
+            return result;
+        }
+
+        public IEnumerator<T> GetEnumerator()
+        {
+            return words.GetEnumerator();
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return words.GetEnumerator();
+        }
+    }
+}
---
+++ HWLesson26/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HWLesson26
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyProtectedUniqueList<string> myList = new MyProtectedUniqueList<string>("love");
+
+            myList.Add("Enjoy");
+            myList.Add("every");
+            myList.Add("moment");
+
+            //myList.Add("Enjoy");
+            //myList.Remove("f");
+            //myList.Clear("e");
+            myList.Clear("love");
+            
+            myList.Add("z");
+            myList.Add("y");
+            myList.Add("x");
+
+            //myList.Sort("qq");
+            myList.Sort("love");
+
+            //myList.RemoveAt(-1);
+            Console.WriteLine(myList);
+        }
+    }
+}
